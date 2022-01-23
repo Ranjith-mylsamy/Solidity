98be1b5758e9617622bf90f19a1d7332cbe1fc93
Ranjith <ranjith.19it@kct.ac.in>
2022-01-23 23:44:21 +0530

    trying hello world

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -1,19 +1,21 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const fs = require("fs");
-
 module.exports = {
-    // See <http://truffleframework.com/docs/advanced/configuration>
-    // to customize your Truffle configuration!
-    networks: {
-        development: {
-            host: "127.0.0.1", // Localhost (default: none)
-            port: 8545, // Standard Ethereum port (default: none)
-            network_id: "*" // Any network (default: none)
-        }
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
     },
-    compilers: {
-        solc: {
-            version: "0.7.0"
-        }
+    loc_sample_sample: {
+      network_id: "*",
+      port: 8546,
+      host: "127.0.0.1"
     }
+  },
+  compilers: {
+    solc: {
+      version: "0.7.0"
+    }
+  }
 };
